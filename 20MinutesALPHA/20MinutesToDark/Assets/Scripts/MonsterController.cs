7c91aa02626e2ceeb8838baad058b08c2c64ab1f
Eibho <eibhyor@gmail.com>
2019-03-30 23:00:45 +0000

    Added the monster sprint

--- 20MinutesALPHA/20MinutesToDark/Assets/Scripts/MonsterController.cs
+++ 20MinutesALPHA/20MinutesToDark/Assets/Scripts/MonsterController.cs
@@ -8,6 +8,9 @@ public class MonsterController : MonoBehaviour
     public float followRadius = 10f;
     Transform player;
     NavMeshAgent agent;
+
+    public float sprintSpeed = 5f;
+    public float speed = 3.5f;
     
 
     // Start is called before the first frame update
@@ -24,7 +27,9 @@ public class MonsterController : MonoBehaviour
         // float distance = Vector3.Distance(player.position, transform.position);
 
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        agent.SetDestination(player.position); 
+        agent.SetDestination(player.position);
+
+        Sprint();
         
     }
 
@@ -33,5 +38,22 @@ public class MonsterController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, followRadius);
+
+        
+    }
+
+    void Sprint()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            GetComponent<NavMeshAgent>().speed = sprintSpeed;
+        }
+        else if (Input.GetKey(KeyCode.RightShift))
+        {
+            GetComponent<NavMeshAgent>().speed = sprintSpeed;
+        }
+
+        else
+            GetComponent<NavMeshAgent>().speed = speed;
     }
 }
