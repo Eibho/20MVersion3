54d05bb5fdc448f0f292dcd7670b86223bfe4886
Eibho <eibhyor@gmail.com>
2019-05-07 15:52:39 +0100

    Added Vignettes

---
+++ 20MinutesALPHA/20MinutesToDark/Assets/Scripts/UI/Vignette.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vignette : MonoBehaviour
+{
+    public Image image;
+
+    void Start()
+    {
+        var tempColor = image.color;
+        tempColor.a = 0.5f;
+        image.color = tempColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
