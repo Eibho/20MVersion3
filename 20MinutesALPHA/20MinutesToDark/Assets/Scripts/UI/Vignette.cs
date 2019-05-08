7407f684e5d8d94ac25f4376d550d298981bed01
Eibho <eibhyor@gmail.com>
2019-05-08 20:04:42 +0100

    Finished Vignettes

---
+++ 20MinutesALPHA/20MinutesToDark/Assets/Scripts/UI/Vignette.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vignette : MonoBehaviour
+{
+    public GameObject image;
+    GameObject player;
+
+    public float theDistance = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distance <= theDistance)
+        {
+            image.SetActive(true);
+        }
+        else
+            image.SetActive(false);
+    }
+}
