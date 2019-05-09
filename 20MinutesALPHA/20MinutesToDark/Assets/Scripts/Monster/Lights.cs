8a9564a26e48e6063c4f158c654ba7f3110b9241
Eibho <eibhyor@gmail.com>
2019-05-09 11:09:30 +0100

    Added UI Help on sun

--- 20MinutesALPHA/20MinutesToDark/Assets/Scripts/Monster/Lights.cs
+++ 20MinutesALPHA/20MinutesToDark/Assets/Scripts/Monster/Lights.cs
@@ -14,6 +14,7 @@ public class Lights : MonoBehaviour
     {
         monster = GameObject.FindGameObjectWithTag("Enemy");
         anim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
